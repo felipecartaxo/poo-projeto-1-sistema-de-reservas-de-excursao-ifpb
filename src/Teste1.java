c3c827caafd77eba1d77a373b622fa119081b3db
Neraky <felipecartaxo1@gmail.com>
2023-10-01 12:58:04 -0300

    Inclusão de testes com outro objeto (excursao2) para testar o método 'gravar()'

--- src/Teste1.java
+++ src/Teste1.java
@@ -6,6 +6,7 @@ public class Teste1 {
 	public static void main(String[] args) {
 		try {
 			Excursao excursao = new Excursao(1234, 100.0, 20);
+			System.out.println("Excursao: " + excursao);
 
 			excursao.criarReserva("111", "joao");
 			excursao.criarReserva("222", "maria");
@@ -14,56 +15,47 @@ public class Teste1 {
 			excursao.criarReserva("333", "ana");
 			excursao.criarReserva("555", "antonio");
 			excursao.criarReserva("555", "joana");
-//			excursao.criarReserva("666", "joao");			
-			excursao.salvar();
 			
-			System.out.println("Excursão - Nota fiscal:\n" + "-------------------------\n" + excursao);
-			System.out.println("-------------------------");
+			System.out.println("Gravar dados no arquivo");
+			excursao.gravar();
 			
-			System.out.println(excursao.listarReservasPorCpf(""));
-			System.out.println("-------------------------");
-			System.out.println(excursao.listarReservasPorCpf("333"));
-			System.out.println("-------------------------");
-			System.out.println(excursao.listarReservasPorNome("paulo"));
-			System.out.println("-------------------------");
-			System.out.println(excursao.getReservas());
-			System.out.println("-------------------------");
+			System.out.println("Inserir reservas após a criação do arquivo");
+			excursao.criarReserva("444", "felipe");
+			excursao.gravar();
+			System.out.println("------------------------------------------");
 			
-//			Excursao excursao2 = new Excursao(999, 888, 5);
-//			System.out.println("excursao:" + excursao2);
-//			
-//			excursao2.criarReserva("912", "joao");
-//			excursao2.criarReserva("913", "maria");
-//			excursao2.criarReserva("914", "felipe");
-////			excursao2.cancelarReserva("914");
-//			
-//			excursao2.salvar();
-//			System.out.println(excursao2);
-//			
+			Excursao excursao2 = new Excursao(5678, 350, 8);
+			System.out.println("Excursão 2: " + excursao2);
+			
+			excursao2.criarReserva("1", "fulano");
+			excursao2.criarReserva("2", "cicrano");
+			excursao2.criarReserva("3", "beltrano");
+			excursao2.gravar();
+			System.out.println("Gravando reservas");
+			System.out.println("Imprimindo Excursao2: ");
+			excursao.listarReservasPorCpf("");
+			
+			excursao2.cancelarReserva("2");
+			excursao2.gravar();
+			System.out.println("Excursao 2: " + excursao2);
 //
 //			System.out.println("\nlistar todas as reservas");
 //			System.out.println(excursao.listarReservasPorCpf(""));
 //			System.out.println("\nlistar as reservas por cpf");
-//			System.out.println(excursao.listarReservasPorCpf("3"));
+//			System.out.println(excursao.listarReservasPorCpf("333"));
 //			System.out.println("\nlistar as reservas por nome");
-////			System.out.println(excursao.listarReservasPorNome("joao"));
-////
-////			excursao.cancelarReserva("555", "claudia");
-////			excursao.cancelarReserva("111");
-//			excursao.cancelarReserva("555", "joana");
-////			System.out.println(excursao);
-////
-////
+//			System.out.println(excursao.listarReservasPorNome("jo"));
+//
+//			// excursao.cancelarReserva("555", "claudia");
+//			excursao.cancelarReserva("333");
+//
 //			System.out.println("\nlistar todas as reservas");
-//			System.out.println(excursao.listarReservasPorCpf("222"));
-//			System.out.println("Listando todas as reservas novamente:");
-//			System.out.println("-----------------------------");
-//			System.out.println(excursao.listarReservasPorNome("antonio"));
+//			System.out.println(excursao.listarReservasPorCpf(""));
 //			System.out.println("\nexcursao:" + excursao);
 //			System.out.println("\ntotal=" + excursao.calcularValorTotal());
-		}
-		catch (Exception erro) {
+		} catch (Exception erro) {
 			System.out.println("-->" + erro.getMessage());
 		}
+
 	}
 }

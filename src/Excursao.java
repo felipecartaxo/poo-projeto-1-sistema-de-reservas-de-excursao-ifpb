82739dacf281b612c083215a6275597cce55c004
Neraky <felipecartaxo1@gmail.com>
2023-10-01 20:50:30 -0300

    Adição do método carregar ()

--- src/Excursao.java
+++ src/Excursao.java
@@ -2,108 +2,87 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Excursao {
 
-	// Atributos
 	private int codigo;
 	private double preco;
 	private int maxReservas;
-	// Array que armazenará as reservas
-	private ArrayList<String> reservas = new ArrayList<>();
 
-	// Construtor com 1 parâmetro
-	public Excursao(int codigo) throws Exception {
-		// Lança uma exceção se 'codigo' <= 0
-		if (codigo <= 0)
+	private ArrayList<String> reservas = new ArrayList<>();		// Array que armazenará as reservas
+
+	public Excursao(int codigo) throws Exception {				// Construtor com 1 parâmetro
+		carregar();
+		if (codigo <= 0) 																// Lança uma exceção se 'codigo' <= 0
+
 			throw new Exception("O código da excursão deve ser maior do que 0");
-		// Caso não seja lançada a exceção
-		this.codigo = codigo;
+		this.codigo = codigo;									// Caso não seja lançada a exceção
+
 	}
 
-	// Construtor com 3 parâmetros
-	public Excursao(int codigo, double preco, int maxReservas) throws Exception {
-		// Lança uma exceção se 'codigo' <= 0
-		if (codigo <= 0)
+	public Excursao(int codigo, double preco, int maxReservas) throws Exception { 		// Construtor com 3 parâmetros
+		if (codigo <= 0) 																// Lança uma exceção se 'codigo' <= 0
 			throw new Exception("O código da excursão deve ser maior do que 0");
-		// Lança uma exceção se 'preco' <= 0
-		if (preco <= 0)
+		if (preco <= 0) 																// Lança uma exceção se 'preco' <= 0
 			throw new Exception("O preço deve ser maior do que 0");
-		// Lança uma exceção se 'maxReservas' <= 0
-		if (maxReservas <= 0)
+		if (maxReservas <= 0) 															// Lança uma exceção se 'maxReservas' <= 0
 			throw new Exception("O número de reservas deve ser maior do que 0");
+
 		// Caso não seja lançada nenhuma exceção
 		this.codigo = codigo;
 		this.preco = preco;
 		this.maxReservas = maxReservas;
 
-		// Criando ArrayList com espaços "disponivel" de acordo com o tamanho informado
-		for (int i = 0; i < maxReservas; i++) {
+		for (int i = 0; i < maxReservas; i++) { 										// Criando ArrayList com espaços "disponivel" de acordo com o tamanho informado
 			reservas.add("disponivel");
 		}
 	}
 
 	public void criarReserva(String cpf, String nome) throws Exception {
-		// Variável auxiliar que será utilizada para retornar (ou não) o índice da
-		// primeira vaga disponível encontrada
-		int vagaDisponivelEncontrada = -1;
-		// Variável que será retornará o índice do primeiro elemento "disponível
-		// encontrado
-		int reservaAtualIndex = 0;
-		// for-each para percorrer todos os itens de 'reservas'
-		for (String reservaAtual : reservas) {
-			// Cada reserva é dividida em duas partes usando "/" como separador e armazenada
-			// no array 'partes'
-			String[] partes = reservaAtual.split("/");
-			// Está verificando se o tamanho do array 'partes' é igual a 2 (para ignorar os
-			// casos onde há "disponivel") e se o elemento na posição [1] é igual ao nome
-			// passado como parâmetro
-			if (partes.length == 2 && partes[1].equals(nome)) {
-				// Se um nome duplicado for encontrado, lançará uma exceção
-				throw new Exception("Nome duplicado na reserva");
+		int vagaDisponivelEncontrada = -1; 												// Variável auxiliar que será utilizada para retornar (ou não) o índice da primeira vaga disponível encontrada
+		int reservaAtualIndex = 0; 														// Variável que será retornará o índice do primeiro elemento "disponível encontrado
+		for (String reservaAtual : reservas) { 											// for-each para percorrer todos os itens de 'reservas'
+			String[] partes = reservaAtual.split("/"); 							// Cada reserva é dividida em duas partes usando "/" como separador e armazenada no array 'partes'
+			if (partes.length == 2 && partes[1].equals(nome)) { 						// Está verificando se o tamanho do array 'partes' é igual a 2 (para ignorar os casos onde há "disponivel") e se o elemento na posição [1] é igual ao nome passado como parâmetro
+				throw new Exception("Nome duplicado na reserva"); 				// Se um nome duplicado for encontrado, lançará uma exceção
 			}
 		}
-		// Itera sobre cada reserva no ArrayList 'reservas'
-		for (String reservaAtual : reservas) {
-			// Ao encontrar o primeiro elemento "disponível", retorna o índice do mesmo
-			if (reservaAtual.equals("disponivel")) {
+
+		for (String reservaAtual : reservas) { 											// Itera sobre cada reserva no ArrayList 'reservas'
+			if (reservaAtual.equals("disponivel")) { 							// Ao encontrar o primeiro elemento "disponível", retorna o índice do mesmo
 				vagaDisponivelEncontrada = reservaAtualIndex;
-				// Sai do loop
-				break;
+				
+				break; 																	// Sai do loop
 			}
-			// Incrementa o índice da variável auxiliar
-			reservaAtualIndex++;
+			
+			reservaAtualIndex++; 														// Incrementa o índice da variável auxiliar
 		}
-		// Se não foi encontrada uma vaga disponivel (ou seja, se 'vagaDisponivelEncontrada' continua sendo igual a -1) lançamos uma exceção
-		if (vagaDisponivelEncontrada == -1) {
+	
+		if (vagaDisponivelEncontrada == -1) {											// Se não foi encontrada uma vaga disponivel (ou seja, se 'vagaDisponivelEncontrada' continua sendo igual a -1) lançamos uma exceção
 			throw new Exception("Todas as vagas estão ocupadas");
 		}
-		// Adiciona a reserva na vaga disponivel encontrada
-		reservas.set(vagaDisponivelEncontrada, cpf + "/" + nome);
+		
+		reservas.set(vagaDisponivelEncontrada, cpf + "/" + nome); 						// Adiciona a reserva na vaga disponivel encontrada
 	}
 
-	// Remove uma reserva passando os campos "cpf/nome"
-	public void cancelarReserva(String cpf, String nome) throws Exception {
-		// Variável de controle para verificar se a reserva foi encontrada
-		boolean reservaEncontrada = false;
-		// Varre a ArrayList 'reservas'
-		for (int i = 0; i < reservas.size(); i++) {
-			// Como o método 'split' é um método de String, iremos armazenar cada elemento de 'reservas' como String
-			String reservaAtual = reservas.get(i);
-			// E em seguida, no array partes, iremos quebrá-los com o método split
-			String[] partes = reservaAtual.split("/");
-			// Se o ArrayList 'reservas' possui o cpf (partes[0]) e o nome (partes[1]), a reserva em questão deverá ser removida
-			if (partes.length == 2 && partes[0].equals(cpf) && partes[1].equals(nome)) {
-				// Remove a reserva caso a mesma seja encontrada
-				reservas.remove(i);
-				// Altera o valor da variável de controle
-				reservaEncontrada = true;
-				// Sai do loop após a remoção
-				break;
+	public void cancelarReserva(String cpf, String nome) throws Exception { 			// Remove uma reserva passando os campos "cpf/nome"
+
+		boolean reservaEncontrada = false; 												// Variável de controle para verificar se a reserva foi encontrada
+		
+		for (int i = 0; i < reservas.size(); i++) { 									// Varre a ArrayList 'reservas'
+			String reservaAtual = reservas.get(i); 										// Como o método 'split' é um método de String, iremos armazenar cada elemento de 'reservas' como String
+			String[] partes = reservaAtual.split("/"); 							// E em seguida, no array partes, iremos quebrá-los com o método split
+			
+			if (partes.length == 2 && partes[0].equals(cpf) && partes[1].equals(nome)) { // Se o ArrayList 'reservas' possui o cpf (partes[0]) e o nome (partes[1]), a reserva em questão deverá ser removida
+				reservas.remove(i); 													// Remove a reserva caso a mesma seja encontrada
+				reservaEncontrada = true; 												// Altera o valor da variável de controle
+				
+				break; 																	// Sai do loop após a remoção
 			}
 		}
-		// Caso a reserva não seja encontrada, lança uma exceção
-		if (!reservaEncontrada) {
+
+		if (!reservaEncontrada) { 														// Caso a reserva não seja encontrada, lança uma exceção
 			throw new Exception("CPF e/ou nome inexistente(s)");
 		}
 	}
@@ -112,7 +91,8 @@ public class Excursao {
 	public void cancelarReserva(String cpf) throws Exception {
 		// Variável de controle para verificar se a reserva foi encontrada
 		boolean cpfEncontrado = false;
-		// ArrayList auxiliar para onde as reservas NÃO RELACIONADAS com o cpf serão armazenadas
+		// ArrayList auxiliar para onde as reservas NÃO RELACIONADAS com o cpf serão
+		// armazenadas
 		ArrayList<String> reservasNaoRemovidas = new ArrayList<>();
 		// Varre a ArrayList 'reservas'
 		for (String reservaAtual : reservas) {
@@ -122,7 +102,8 @@ public class Excursao {
 				// Altera o valor da variável de controle
 				cpfEncontrado = true;
 			} else {
-				// Caso a reserva em questão não esteja associada ao CPF, mantém a reserva na lista de reservas não removidas
+				// Caso a reserva em questão não esteja associada ao CPF, mantém a reserva na
+				// lista de reservas não removidas
 				reservasNaoRemovidas.add(reservaAtual);
 			}
 		}
@@ -142,12 +123,14 @@ public class Excursao {
 		}
 		// Caso contrário, iremos procurar se alguma reserva bate com 'cpf'
 		else {
-			// ArrayList auxiliar para armazenar as reservas que batem com o 'cpf' passado no método
+			// ArrayList auxiliar para armazenar as reservas que batem com o 'cpf' passado
+			// no método
 			ArrayList<String> reservasPorCpf = new ArrayList<>();
 			for (String reservaAtual : reservas) {
 				String[] partes = reservaAtual.split("/");
-				if(partes.length == 2 && partes[0].equals(cpf)) {
-					// Caso alguma reserva possua o 'cpf' igual, esta reserva será adicionada ao ArrayList auxiliar
+				if (partes.length == 2 && partes[0].equals(cpf)) {
+					// Caso alguma reserva possua o 'cpf' igual, esta reserva será adicionada ao
+					// ArrayList auxiliar
 					reservasPorCpf.add(reservaAtual);
 				}
 			}
@@ -186,42 +169,73 @@ public class Excursao {
 	}
 
 	// Gravar no arquivo “codigo.txt” o preço, maxReservas e as reservas
-	public void salvar() throws IOException {
-		// Grava as reservas no arquivo 'codigo.csv' que será criado no diretório atual
-		File f = new File(new File(".\\" + getCodigo() + ".csv").getCanonicalPath()); // pasta do projeto
-		// O objeto 'arq' é criado para escrever no arquivo representado por f. O argumento 'true' indica que novos dados serão adicionados ao final do arquivo, preservando os dados existentes
-		FileWriter arq = new FileWriter(f, true);
-		// Contém as informações solicitadas pelo professor que deveriam ser gravadas no arquivo .csv
-		arq.write(getPreco() + "; " + getMaxReservas() + "; " + getReservas() + "\n");
-		// Fecha o arquivo
-		arq.close();
-	}
+//	public void salvar() throws IOException {
+//		// Grava as reservas no arquivo 'codigo.csv' que será criado no diretório atual
+//		File f = new File(new File(".\\" + getCodigo() + ".csv").getCanonicalPath()); // pasta do projeto
+//		// O objeto 'arq' é criado para escrever no arquivo representado por f. O
+//		// argumento 'true' indica que novos dados serão adicionados ao final do
+//		// arquivo, preservando os dados existentes
+//		FileWriter arq = new FileWriter(f, true);
+//		// Contém as informações solicitadas pelo professor que deveriam ser gravadas no
+//		// arquivo .csv
+//		arq.write(getPreco() + "; " + getMaxReservas() + "; " + getReservas() + "\n");
+//		// Fecha o arquivo
+//		arq.close();
+//	}
 
 	// Ler do arquivo “codigo.txt” o preço, maxReservas e as reservas
-	public void carregar() {
+	public void carregar() throws Exception {
+		try {
+			File f = new File(new File(".\\" + getCodigo() + ".csv").getCanonicalPath());
+			String linha;
+			String[] partes;
 
+			Scanner arquivo = new Scanner(f);
+			String cabecalho =  arquivo.nextLine();
+			System.out.println(cabecalho);
+			
+			while(arquivo.hasNextLine()) {
+				linha = arquivo.nextLine();
+				partes = linha.split(";");
+				if (!partes[2].equals("disponivel")) {
+					String[] partesReserva = partes[2].split("/");
+					criarReserva(partesReserva[0], partesReserva[1]);
+				}
+			}
+			
+		}
+		catch (IOException e) {
+			File f = new File(new File(".\\" + getCodigo() + ".csv").getCanonicalPath());
+			FileWriter arquivo2 = new FileWriter(f, false);
+			arquivo2.close();
+		}
 	}
-	
+
 	// Grava os dados no arquivo .csv
 	public void gravar() throws Exception {
 		try {
-			// Tenta abrir o arquivo "codigo.csv", localizado na pasta do projeto, usando um objeto 'File'
+			// Tenta abrir o arquivo "codigo.csv", localizado na pasta do projeto, usando um
+			// objeto 'File'
 			File f = new File(new File(".\\" + getCodigo() + ".csv").getCanonicalPath());
-			// Em seguida, utiliza um 'FileWriter' para escrever no arquivo. O parâmetro 'false' indica que o arquivo deve ser aberto para escrita, substituindo o conteúdo anterior
+			// Em seguida, utiliza um 'FileWriter' para escrever no arquivo. O parâmetro
+			// 'false' indica que o arquivo deve ser aberto para escrita, substituindo o
+			// conteúdo anterior
 			FileWriter arquivo = new FileWriter(f, false);
-			// Grava a primeira linha do arquivo (linha de cabecalho contendo o Preço e o Nº máximo de reservas da excursão)
+			// Grava a primeira linha do arquivo (linha de cabecalho contendo o Preço e o Nº
+			// máximo de reservas da excursão)
 			arquivo.write("Preço; Max; Reserva\n");
 			// Variável responsável por armazenar a reserva
 			String reservaAtual;
-			// Percorre o ArrayList 'reservas', verificando se uma reserva está ocupada (ou seja, não é "disponivel"). Se uma reserva estiver ocupada, escreve o número da reserva e a o registro correspondente no arquivo .csv
+			// Percorre o ArrayList 'reservas', verificando se uma reserva está ocupada (ou
+			// seja, não é "disponivel"). Se uma reserva estiver ocupada, escreve o número
+			// da reserva e a o registro correspondente no arquivo .csv
 			for (int i = 0; i < reservas.size(); i++) {
 				reservaAtual = reservas.get(i);
 				// Gravar no arquivo “codigo.txt” o preço, max e as reservas
 				arquivo.write(getPreco() + "; " + getMaxReservas() + "; " + reservaAtual + "\n");
 			}
 			arquivo.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new Exception("Problema na gravação do arquivo");
 		}
 	}
